58c4775801c64b858a5d84be86e35470d7bb7a15
Said <shahrokh07.96@gmail.com>
2023-08-17 00:05:14 +0530

    intro-section-with-dropdown-navigation project in progress

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.html";
+import "../styles/styles.scss";
+import navigation from "./components/navigation.js";
+import home from "./components/home.js";
+(function(){
+    navigation;
+    home;
+})();
