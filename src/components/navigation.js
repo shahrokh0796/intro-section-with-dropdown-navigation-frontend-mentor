8a9abf46309738feeb48b02904ca9b584ae47015
Said <shahrokh07.96@gmail.com>
2023-10-25 15:08:59 +0530

    project completed with improved accibility intro-section-with dropdown menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,19 @@
 import openNav from "../images/icon-close-menu.svg";
 import closeNav from "../images/icon-menu.svg";
-import arrowUp from "../images/icon-arrow-up.svg";
-import arrowDown from "../images/icon-arrow-down.svg";
+
 
  
 const navbar = document.querySelector(".navbar");
-const main = document.querySelector("main");
-const body = document.body;
+const backgroundShadow = document.querySelector(".navshadow");
 const navToggle = document.querySelector(".nav-toggle");
+const dropFeatures = document.querySelector(".btn-dropdown-fe");
+const dropCompany = document.querySelector(".btn-dropdown-co");
+const subMenus = document.querySelectorAll(".sub-menu");
 const burgerImg = navToggle.firstElementChild.children[0];
 const navExpanded = navToggle.querySelector("button");
-console.log(navExpanded.ariaExpanded);
+
+
+
 function addNavBackShadow(ele) {
     let bodey = ele;
     bodey.classList.add("backgroundShadow");
@@ -26,12 +29,56 @@ navToggle.addEventListener("click", (e) => {
     if (burgerImg.src === `${openNav}`) {
         burgerImg.src = `${closeNav}`;
         navExpanded.ariaExpanded = false;
-        removNavBackShadow(body);
-        // removNavBackShadow(main);
+        backgroundShadow.classList.add("display-none");
+        removNavBackShadow(backgroundShadow);
     } else {
         burgerImg.src = `${openNav}`;
         navExpanded.ariaExpanded = true;
-        addNavBackShadow(body);
-        // addNavBackShadow(main);
+        backgroundShadow.classList.remove("display-none");
+        addNavBackShadow(backgroundShadow);
     }
 });
+
+function expandAria (elem) {
+    elem.ariaExpanded = true;
+}
+
+function collapseAria (elem) {
+    elem.ariaExpanded = false;
+}
+
+function dropdownAccessibility (ele, index) {
+    if (ele[index].classList.contains("display-block")) {
+        ele[index].classList.remove("display-block");
+        collapseAria(dropFeatures);
+        collapseAria(dropCompany);
+        console.log(dropFeatures.getAttribute("aria-expanded"));
+        // ele[index].classList.add("display-none");
+        // ele[index].ariaExpanded = "false";
+    } else {
+        ele[index].classList.add("display-block");
+        expandAria(dropFeatures);
+        expandAria(dropCompany);
+        console.log(dropFeatures.getAttribute("aria-expanded"));
+        // ele[index].classList.remove("display-none");
+        // ele[index].ariaExpanded ="true";
+        // console.log(ele[index].ariaExpanded);
+    }
+    // if (ele[index].style.display === "block") {
+    //     ele[index].style.display = "none";
+    //     console.log()
+    //     ele[index].ariaExpanded = "false";
+    // } else {
+    //     ele[index].style.display = "block";
+    //     ele[index].ariaExpanded = "true";
+    // }
+    
+}
+
+dropFeatures.addEventListener("click", (e) => {
+    dropdownAccessibility(subMenus, 0);
+});
+dropCompany.addEventListener("click", (e) => {
+    dropdownAccessibility(subMenus, 1);
+});
+
